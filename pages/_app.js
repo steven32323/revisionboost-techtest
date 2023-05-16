a87e3bb5a8418dd5dbe28bdfd511c46371d15f1e
Jake <Jake@revisionboost.com>
2023-05-16 16:47:50 +0100

    Random Question Task

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { evaluateTex } from 'tex-math-parser';
 export default function App({}){
 
     const [memory, setMemory] = useState({});
+    const [solutionShown, setSolutionShown] = useState(false);
 
     function addToMemory(newValue){
         setMemory((prev)=>{
@@ -21,10 +22,16 @@ export default function App({}){
                 <StaticMath latex={`\\text{Generate a random question and display it here.}`} />
                 <br/>
                 <br/>
+                {solutionShown ? <StaticMath latex={`\\text{Display the solution here}`} /> : ''}
+                <br/>
+                <br/>
                 <MathInput buttons={['power', 'times']} markingFunction={markingFunction} memKey='mathinput1' memory={memory} setMemory={addToMemory} placeholder="Type your answer here!"/>
                 <br/>
                 <br/>
                 <button onClick={()=>{setMemory((prev)=>{return{...prev, feedbackShown:true}})}}>Check Answer</button>
+                <br/>
+                {!solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(true)}}>Show Solution</button> : ''}
+                {solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(false)}}>Hide Solution</button> : ''}
             </div>
         </div>
     );
