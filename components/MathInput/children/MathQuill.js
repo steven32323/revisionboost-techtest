91983060d12ae8e6ecaf127a11080a2d17b27342
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-05-22 09:54:28 +0100

    initial commit

--- components/MathInput/children/MathQuill.js
+++ components/MathInput/children/MathQuill.js
@@ -1,38 +1,53 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { addStyles, EditableMathField, StaticMathField } from 'react-mathquill'
-import styles from '../../../public/styles/MathQuill.module.css'
-import thickWhiteCross from '../../../public/thickWhiteCross.svg'
-import thickWhiteTick from '../../../public/thickWhiteTick.svg'
-import Image from 'next/image'
-import { useTransition, animated} from 'react-spring';
+import React, { useState, useRef, useEffect } from "react";
+import { addStyles, EditableMathField, StaticMathField } from "react-mathquill";
+import styles from "../../../public/styles/MathQuill.module.css";
+import thickWhiteCross from "../../../public/thickWhiteCross.svg";
+import thickWhiteTick from "../../../public/thickWhiteTick.svg";
+import Image from "next/image";
+import { useTransition, animated } from "react-spring";
 
 // inserts the required css to the <head> block.
 addStyles();
 
-export default function MathQuill({buttonArray, markingFunction, memKey, memory, setMemory, defaultValue, placeholder, marks, dependantMarking, answerCompiler}){
-
-  if(!memKey){
-    throw new Error('Each MathInput component needs a unique memKey for storing quetsion data in memory');
+export default function MathQuill({
+  buttonArray,
+  markingFunction,
+  memKey,
+  memory,
+  setMemory,
+  defaultValue,
+  placeholder,
+  marks,
+  dependantMarking,
+  answerCompiler,
+}) {
+  if (!memKey) {
+    throw new Error(
+      "Each MathInput component needs a unique memKey for storing quetsion data in memory"
+    );
   }
 
   const [firstRender, setFirstRender] = useState(true);
 
-  const marksEntry = useTransition(memory.feedbackShown, firstRender ? {enter:{x:'0px'}, leave:{x:`57px`}}
-    :
-    {
-      from:{x:`57px`},
-      enter:{x:'0px'},
-      leave:{x:`57px`}
-    });
+  const marksEntry = useTransition(
+    memory.feedbackShown,
+    firstRender
+      ? { enter: { x: "0px" }, leave: { x: `57px` } }
+      : {
+          from: { x: `57px` },
+          enter: { x: "0px" },
+          leave: { x: `57px` },
+        }
+  );
 
   let initialInput;
-  if(memory[memKey] && memory[memKey].defaultValue){
-      initialInput = memory[memKey].defaultValue;
-  }else{
-    if(defaultValue){
-      initialInput=defaultValue;
-    }else{
-      initialInput='';
+  if (memory[memKey] && memory[memKey].defaultValue) {
+    initialInput = memory[memKey].defaultValue;
+  } else {
+    if (defaultValue) {
+      initialInput = defaultValue;
+    } else {
+      initialInput = "";
     }
   }
 
@@ -40,155 +55,326 @@ export default function MathQuill({buttonArray, markingFunction, memKey, memory,
   const [buttonsShown, setButtonsShown] = useState(false);
 
   //All instances of the index 0 latex will be instantly replaced with the index 1 latex:
-  const substitutions = [ ['\\cdot', '\\times'], ['\\text', '']];
+  const substitutions = [
+    ["\\cdot", "\\times"],
+    ["\\text", ""],
+  ];
   const mathFieldRef = useRef({});
 
   const config = {
     spaceBehavesLikeTab: true,
-    leftRightIntoCmdGoes: 'up',
+    leftRightIntoCmdGoes: "up",
     restrictMismatchedBrackets: true,
     sumStartsWithNEquals: true,
     supSubsRequireOperand: true,
-    charsThatBreakOutOfSupSub: '+-=<>*',
+    charsThatBreakOutOfSupSub: "+-=<>*",
     autoSubscriptNumerals: true,
-    autoCommands: 'pi sqrt div',
+    autoCommands: "pi sqrt div",
     maxDepth: 10,
-  }
+  };
 
-
-
-  
-  function changeHandler(mathField){
+  function changeHandler(mathField) {
     mathFieldRef.current = mathField;
     let newLatex = mathField.latex();
 
-    for(let i = 0; i<substitutions.length; i++){
-        newLatex = newLatex.replaceAll(substitutions[i][0], substitutions[i][1]);
+    for (let i = 0; i < substitutions.length; i++) {
+      newLatex = newLatex.replaceAll(substitutions[i][0], substitutions[i][1]);
     }
 
     setLatex(newLatex);
 
-    if(!dependantMarking){
+    if (!dependantMarking) {
       let score;
-      if(Object.keys(memory).length > 1){
+      if (Object.keys(memory).length > 1) {
         score = markingFunction(evaluateSafe(newLatex), memory);
-      }else{
+      } else {
         score = 0;
       }
       setMemory({
-        [memKey]:{
+        [memKey]: {
           score: score,
           marks: marks,
-          defaultValue: newLatex
+          defaultValue: newLatex,
         },
       });
-    }else{
+    } else {
       answerCompiler(evaluateSafe(newLatex), memKey);
     }
-
   }
 
-  function mountingHandler(mathField){
+  function mountingHandler(mathField) {
     mathFieldRef.current = mathField;
   }
 
   //makes the latex compatible with the tex-math-parser plugin
-  function evaluateSafe(latex){
-    latex = latex.replaceAll('\\times', '\\cdot');
+  function evaluateSafe(latex) {
+    latex = latex.replaceAll("\\times", "\\cdot");
     return latex;
   }
 
   //Button Handlers:
-  function coordinates(){
-    mathFieldRef.current.cmd('('); 
-    mathFieldRef.current.cmd('x'); 
-    mathFieldRef.current.cmd(','); 
-    mathFieldRef.current.cmd(' '); 
-    mathFieldRef.current.cmd('y'); 
-    mathFieldRef.current.cmd(')'); 
+  function coordinates() {
+    mathFieldRef.current.cmd("(");
+    mathFieldRef.current.cmd("x");
+    mathFieldRef.current.cmd(",");
+    mathFieldRef.current.cmd(" ");
+    mathFieldRef.current.cmd("y");
+    mathFieldRef.current.cmd(")");
     mathFieldRef.current.focus();
   }
 
-  function power(){
-    mathFieldRef.current.cmd('^'); 
+  function power() {
+    mathFieldRef.current.cmd("^");
     mathFieldRef.current.focus();
   }
 
-  function vector_a(){
+  function vector_a() {
     mathFieldRef.current.focus();
     mathFieldRef.current.write("\\mathrm{a}");
     mathFieldRef.current.focus();
   }
 
-  function vector_b(){
+  function vector_b() {
     mathFieldRef.current.focus();
     mathFieldRef.current.write("\\mathrm{b}");
     mathFieldRef.current.focus();
   }
 
-  function frac(){
-    mathFieldRef.current.cmd('/'); 
+  function frac() {
+    mathFieldRef.current.cmd("/");
     mathFieldRef.current.focus();
   }
 
-  function sqrt(){
-    mathFieldRef.current.cmd('\\sqrt'); 
+  function sqrt() {
+    mathFieldRef.current.cmd("\\sqrt");
     mathFieldRef.current.focus();
   }
 
-  function times(){
-    mathFieldRef.current.typedText('*');
+  function times() {
+    mathFieldRef.current.typedText("*");
     mathFieldRef.current.focus();
   }
 
-  function percentage(){
-    mathFieldRef.current.typedText('%');
+  function percentage() {
+    mathFieldRef.current.typedText("%");
     mathFieldRef.current.focus();
   }
 
-  function pound(){
-    mathFieldRef.current.typedText('£');
+  function pound() {
+    mathFieldRef.current.typedText("£");
     mathFieldRef.current.focus();
   }
 
-  useEffect(()=>{
-    if(typeof memory[memKey] === 'undefined'){
+  useEffect(() => {
+    if (typeof memory[memKey] === "undefined") {
       setMemory({
-        [memKey] : {}
+        [memKey]: {},
       });
     }
-    if(firstRender){
+    if (firstRender) {
       setFirstRender(false);
     }
-  },[]);
-
+  }, []);
 
   return (
     <div className="mathInput">
-      <div style={buttonsShown && buttonArray && buttonArray.length > 0 ? {borderRadius: '5px 5px 0px 0px'} : {borderRadius: '5px'}} className={styles.mathFieldWrapper} onFocus={()=>{setButtonsShown(true); if(memory.feedbackShown){setMemory({feedbackShown: false})}}} onBlur={()=>{setButtonsShown(false);}}>
+      <div
+        style={
+          buttonsShown && buttonArray && buttonArray.length > 0
+            ? { borderRadius: "5px 5px 0px 0px" }
+            : { borderRadius: "5px" }
+        }
+        className={styles.mathFieldWrapper}
+        onFocus={() => {
+          setButtonsShown(true);
+          if (memory.feedbackShown) {
+            setMemory({ feedbackShown: false });
+          }
+        }}
+        onBlur={() => {
+          setButtonsShown(false);
+        }}
+      >
         <div className={styles.wrapper}>
-          {memory[memKey] && memory[memKey].score === 1 ? marksEntry((style, item)=>{return item ?   <animated.div style={style} className={styles.correctIcon}><Image alt="correct" src={thickWhiteTick} layout="responsive"/></animated.div> : ''}): marksEntry((style, item)=>{return item ?   <animated.div style={style} className={styles.incorrectIcon}><Image alt="incorrect" src={thickWhiteCross} layout="responsive"/></animated.div> : ''})}
-          <EditableMathField config={config} latex={latex} onChange={changeHandler} mathquillDidMount={mountingHandler}/>
+          {memory[memKey] && memory[memKey].score === 1
+            ? marksEntry((style, item) => {
+                return item ? (
+                  <animated.div style={style} className={styles.correctIcon}>
+                    <Image
+                      alt="correct"
+                      src={thickWhiteTick}
+                      layout="responsive"
+                    />
+                  </animated.div>
+                ) : (
+                  ""
+                );
+              })
+            : marksEntry((style, item) => {
+                return item ? (
+                  <animated.div style={style} className={styles.incorrectIcon}>
+                    <Image
+                      alt="incorrect"
+                      src={thickWhiteCross}
+                      layout="responsive"
+                    />
+                  </animated.div>
+                ) : (
+                  ""
+                );
+              })}
+          <EditableMathField
+            config={config}
+            latex={latex}
+            onChange={changeHandler}
+            mathquillDidMount={mountingHandler}
+          />
         </div>
-        {buttonsShown || latex !== '' ? '' : 
+        {buttonsShown || latex !== "" ? (
+          ""
+        ) : (
           <div className={styles.placeholderWrapper}>
             <p>{placeholder}</p>
           </div>
-        }
+        )}
       </div>
-      {buttonsShown && buttonArray && buttonArray.length > 0 ? 
-        <div onMouseDown={(e)=>{e.preventDefault()}} className={styles.buttonsWrapper}>
-          {buttonArray.includes('power') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={power}><div className={styles.clickBlocker}><StaticMathField>{'a^b'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('sqrt') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={sqrt}><div className={styles.clickBlocker}><StaticMathField>{'\\sqrt{ \\ }'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('times') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={times}><div className={styles.clickBlocker}><StaticMathField>{'\\times'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('%') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={percentage}><div className={styles.clickBlocker}><StaticMathField>{'\\%'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('frac') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={frac}><div className={styles.clickBlocker}><StaticMathField>{'\\frac{a}{b}'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('coordinates') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={coordinates}><div className={styles.clickBlocker}><StaticMathField>{'\\left(x, \\ y\\right)'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('pound') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={pound}><div className={styles.clickBlocker}><StaticMathField>{'£'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('vector_a') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={vector_a}><div className={styles.clickBlocker}><StaticMathField>{'\\mathrm{a}'}</StaticMathField></div></button> : ''}
-          {buttonArray.includes('vector_b') ? <button onMouseDown={(e)=>{e.preventDefault()}} onClick={vector_b}><div className={styles.clickBlocker}><StaticMathField>{'\\mathrm{b}'}</StaticMathField></div></button> : ''}
-        </div> 
-      : ''}
+      {buttonsShown && buttonArray && buttonArray.length > 0 ? (
+        <div
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+          className={styles.buttonsWrapper}
+        >
+          {buttonArray.includes("power") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={power}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"a^b"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("sqrt") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={sqrt}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\sqrt{ \\ }"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("times") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={times}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\times"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("%") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={percentage}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\%"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("frac") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={frac}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\frac{a}{b}"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("coordinates") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={coordinates}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\left(x, \\ y\\right)"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("pound") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={pound}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"£"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("vector_a") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={vector_a}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\mathrm{a}"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+          {buttonArray.includes("vector_b") ? (
+            <button
+              onMouseDown={(e) => {
+                e.preventDefault();
+              }}
+              onClick={vector_b}
+            >
+              <div className={styles.clickBlocker}>
+                <StaticMathField>{"\\mathrm{b}"}</StaticMathField>
+              </div>
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,205 @@
-import StaticMath from '../components/StaticMath/StaticMath'
-import MathInput from '../components/MathInput/MathInput';
-import { useState } from 'react';
-import '../public/styles/globals.css'
-import { evaluateTex } from 'tex-math-parser';
+import StaticMath from "../components/StaticMath/StaticMath";
+import MathInput from "../components/MathInput/MathInput";
+import { useState, useEffect } from "react";
+import "../public/styles/globals.css";
+import { evaluateTex } from "tex-math-parser";
 
-export default function App({}){
+export default function App({}) {
+  // lots of state management, could be improved(useReducer?)
+  const [memory, setMemory] = useState({});
+  const [solutionShown, setSolutionShown] = useState(false);
+  const [hintShown, setHintShown] = useState(false);
+  const [quantity1, setQuantity1] = useState(1);
+  const [quantity2, setQuantity2] = useState(1);
+  const [quantity3, setQuantity3] = useState(1);
+  const [quantity4, setQuantity4] = useState(1);
+  const [totalCost1, setTotalCost1] = useState(1);
+  const [totalCost2, setTotalCost2] = useState(1);
+  const [answer, setAnswer] = useState();
+  const [itemType, setItemType] = useState([]);
+  const [hintFlag, setHintFlag] = useState(0);
+  const items = [
+    {
+      item1: "apple",
+      item2: "carrot",
+      measurement: "kg of",
+      denomination: "pence",
+    },
+    {
+      item1: "TV",
+      item2: "Washing machine",
+      denomination: "pound",
+    },
+    {
+      item1: "cars",
+      item2: "vans",
+      denomination: "pound",
+    },
+  ];
 
-    const [memory, setMemory] = useState({});
-    const [solutionShown, setSolutionShown] = useState(false);
+  useEffect(() => {
+    let q1, q2, q3, q4, tc1, tc2, cost1, cost2;
 
-    function addToMemory(newValue){
-        setMemory((prev)=>{
-            return {...prev, ...newValue}
-        });
-    }
+    // Generate costs of each item
+    cost1 = Math.ceil(Math.random() * 100);
+    cost2 = Math.ceil(Math.random() * 100);
 
-    return(
-        <div style={{display:'flex', justifyContent:'center'}}>
-            <div style={{maxWidth:'800px', width:'calc(100vw - 40px)', marginTop:'50px'}}>
-                <StaticMath latex={`\\text{The <StaticMath /> component can be used to write text inline with latex equations: } x^2 + 3x - 2`} />
-                <StaticMath latex={`\\text{Generate a random question and display it here.}`} />
-                <br/>
-                <br/>
-                {solutionShown ? <StaticMath latex={`\\text{Display the solution here}`} /> : ''}
-                <br/>
-                <br/>
-                <MathInput buttons={['power', 'times']} markingFunction={markingFunction} memKey='mathinput1' memory={memory} setMemory={addToMemory} placeholder="Type your answer here!"/>
-                <br/>
-                <br/>
-                <button onClick={()=>{setMemory((prev)=>{return{...prev, feedbackShown:true}})}}>Check Answer</button>
-                <br/>
-                {!solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(true)}}>Show Solution</button> : ''}
-                {solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(false)}}>Hide Solution</button> : ''}
-            </div>
-        </div>
-    );
+    // Generate quantities of each item (will loop, regenerating until the ratio of quantities of each item is not the same, therefore making the question solveable)
+    do {
+      q1 = Math.round(Math.random() * 10) + 1;
+      q2 = Math.round(Math.random() * 10) + 1;
+      q3 = Math.round(Math.random() * 10) + 1;
+      q4 = Math.round(Math.random() * 10) + 1;
+    } while (q1 / q2 === q3 / q4);
+
+    // Calculate total costs based on quantities and costs
+    tc1 = q1 * cost1 + q2 * cost2;
+    tc2 = q3 * cost1 + q4 * cost2;
+
+    // Calculate the answer to be stored in state later
+    let answer = cost1 + cost2;
+
+    setQuantity1(q1);
+    setQuantity2(q2);
+    setQuantity3(q3);
+    setQuantity4(q4);
+    setTotalCost1(tc1);
+    setTotalCost2(tc2);
+    setAnswer(answer);
+    setItemType(answer < 75 ? items[0] : items[1]);
+    console.log(answer);
+  }, []);
+
+  function addToMemory(newValue) {
+    setMemory((prev) => {
+      return { ...prev, ...newValue };
+    });
+  }
+
+  let itemDenominationPrefix = itemType.denomination === "pound" ? "£" : "";
+  let itemDenominationSuffix = itemType.denomination === "pence" ? "p" : "";
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          maxWidth: "800px",
+          width: "calc(100vw - 40px)",
+          marginTop: "50px",
+        }}
+      >
+        {/* <StaticMath
+          latex={`\\text{The <StaticMath /> component can be used to write text inline with latex equations: } x^2 + 3x - 2`}
+        /> */}
+        <StaticMath
+          latex={`\\text{${quantity1} ${
+            itemType.measurement ? itemType.measurement : ""
+          } ${itemType.item1}s and ${quantity2} ${
+            itemType.item2
+          }s have a total cost of ${itemDenominationPrefix}${totalCost1}${itemDenominationSuffix}. }`}
+        />
+        <br />
+        <StaticMath
+          latex={`\\text{${quantity3} ${
+            itemType.measurement ? itemType.measurement : ""
+          } ${itemType.item1}s and ${quantity4} ${
+            itemType.item2
+          }s have a total cost of ${itemDenominationPrefix}${totalCost2}${itemDenominationSuffix}. }`}
+        />
+        <br />
+        <br />
+        <StaticMath
+          latex={`\\text{Work out the total cost of 1 ${
+            itemType.measurement ? itemType.measurement : ""
+          } ${itemType.item1} and 1 ${
+            itemType.measurement ? itemType.measurement : ""
+          } ${itemType.item2}.}`}
+        />
+        <br />
+        <br />
+        {solutionShown && <StaticMath latex={`\\text{Answer: } ${answer}`} />}
+        <br />
+        <br />
+        {hintShown && (
+          <>
+            <StaticMath
+              latex={`\\text{This is a Linear Equation, here's how you would get started:}`}
+            />
+            <br />
+            <StaticMath
+              latex={`\\text{${quantity1}x + ${quantity2}y = ${totalCost1}}`}
+            />
+            <StaticMath
+              latex={`\\text{${quantity3}x + ${quantity4}y = ${totalCost2}}`}
+            />
+          </>
+        )}
+        <br />
+        <br />
+        <MathInput
+          markingFunction={(userInput) => markingFunction(userInput, answer)}
+          memKey="mathinput1"
+          memory={memory}
+          setMemory={addToMemory}
+          placeholder="Type your answer here!"
+        />
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            setMemory((prev) => {
+              return { ...prev, feedbackShown: true };
+            });
+            setHintFlag((prevState) => (prevState += 1));
+            console.log(hintFlag);
+          }}
+        >
+          Check Answer
+        </button>
+        <br />
+        {!solutionShown && (
+          <button
+            style={{ marginTop: "20px" }}
+            onClick={() => {
+              setSolutionShown(true);
+            }}
+          >
+            Show Solution
+          </button>
+        )}
+        {solutionShown && (
+          <button
+            style={{ marginTop: "20px" }}
+            onClick={() => {
+              setSolutionShown(false);
+            }}
+          >
+            Hide Solution
+          </button>
+        )}
+        <br />
+        <br />
+        {hintFlag >= 4 && (
+          <button onClick={() => setHintShown((prevState) => !prevState)}>
+            {hintShown ? "Hide" : "Show"} Hint
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-function markingFunction(userInput){
-    let inputValue;
-    try{
-        //the evaluateTex function takes a latex string as an input and returns the evaluation as a javascript number
-        inputValue = evaluateTex(userInput).evaluated;
-    }catch{
-        return 0;
-    }
-    if(inputValue === 5){
-        return 1
-    }else{
-        return 0;
-    }
+function markingFunction(userInput, answer) {
+  let inputValue;
+  try {
+    //the evaluateTex function takes a latex string as an input and returns the evaluation as a javascript number
+    inputValue = evaluateTex(userInput).evaluated;
+  } catch {
+    return 0;
+  }
+  if (inputValue === answer) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
